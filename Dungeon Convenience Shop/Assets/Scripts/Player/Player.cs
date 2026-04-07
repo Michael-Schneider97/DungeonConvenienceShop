4fb0371a8d83ba4c92699f3d54aaae6312053c0d
Michael Schneider <michaews@uci.edu>
2026-04-07 08:42:42 -0700

    wasd movement implemented

--- Dungeon Convenience Shop/Assets/Scripts/Player/Player.cs
+++ Dungeon Convenience Shop/Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -6,14 +7,20 @@ public class Player : MonoBehaviour
     [SerializeField] public float walkSpd = 5f;
     [SerializeField] public float runSpd = 10f;
     [SerializeField] public float jump = 5f;
+    [SerializeField] public float grav = -9.81f;
 
     [Header("Player Camera")]
-    [SerializeField] public float mouseSens = 2f;
+    [SerializeField] public float mouseSens = 0.2f;
     [SerializeField] public float camWidth = 90f;
 
     private CharacterController controller;
     private Vector3 velocity;
     private float xRotation = 0f;
+    private Vector2 lookInput;
+    private Vector2 moveInput;
+    private PlayerInput inputActions;
+    private bool isOnGround;
+    private float currentSpd = 0;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -22,39 +29,41 @@ public class Player : MonoBehaviour
         controller = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        inputActions = new PlayerInput();
+        inputActions.Player.Jump.performed += ctx => handleJump();
+        currentSpd = walkSpd;
+    }
 
+    public void OnMove(InputValue val)
+    {
+        moveInput = val.Get<Vector2>();
+    }
+
+    public void OnJump()
+    {
+        if (isOnGround)
+            velocity.y = Mathf.Sqrt(jump * -2f * grav);
+    }
+
+    public void OnSprint(InputValue value)
+    {
+        currentSpd = value.isPressed ? walkSpd : runSpd;
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleMouseLook();
-        HandleMovement();
-        //HandleJump();
-        //HandleGrav();
+        handleSprint();
+
+        isOnGround = controller.isGrounded;
+        if (isOnGround && velocity.y < 0)
+            velocity.y = -2f;
+
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * Time.deltaTime * currentSpd);
+
+        velocity.y += grav * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    private void HandleMouseLook()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSens;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSens;
-        
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -camWidth, camWidth);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        
-        transform.Rotate(Vector3.up * mouseX);
-    }
-
-    private void HandleMovement()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        move.Normalize();
-
-        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpd : walkSpd;
-        controller.Move(move * speed * Time.deltaTime);
-    }
-}
+};
